f9b3c72567ab5718009d06f207fc166b5905ead7
pangratz666 <pangratz666@gmail.com>
2010-10-30 18:01:08 +0200

    added tweets of a tweeter to the model

--- eclipse/twitter/src/main/java/at/jku/semantic/twitter/App.java
+++ eclipse/twitter/src/main/java/at/jku/semantic/twitter/App.java
@@ -2,9 +2,14 @@ package at.jku.semantic.twitter;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.Iterator;
 import java.util.Properties;
 
+import twitter4j.GeoLocation;
 import twitter4j.IDs;
+import twitter4j.Paging;
+import twitter4j.ResponseList;
+import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
 import twitter4j.User;
@@ -28,6 +33,11 @@ import com.hp.hpl.jena.rdf.model.ResourceFactory;
  * 
  */
 public class App {
+
+	public static final String FOAF_NS = "http://xmlns.com/foaf/0.1/Person#";
+	public static final String SEMANTIC_TWEETER_NS = "http://twitter.com/semantic/Tweeter#";
+	public static final String SEMANTIC_TWEET_NS = "http://twitter.com/semantic/Tweet#";
+
 	public static void main(String args[]) throws Exception {
 		Properties props = new Properties();
 		props.load(App.class.getResourceAsStream("/twitter.properties"));
@@ -50,31 +60,44 @@ public class App {
 			User user = twitter.showUser(id);
 
 			Model userModel = ModelFactory.createDefaultModel();
-
-			String foafNS = "http://xmlns.com/foaf/0.1/Person#";
-			userModel.setNsPrefix("foaf", foafNS);
-
-			String stNS = "http://twitter.com/semantic/Tweeter#";
-			userModel.setNsPrefix("st", stNS);
+			userModel.setNsPrefix("foaf", FOAF_NS);
+			userModel.setNsPrefix("stweeter", SEMANTIC_TWEETER_NS);
+			userModel.setNsPrefix("stweet", SEMANTIC_TWEET_NS);
 
 			// NICKNAME
-			Resource usedIdRes = ResourceFactory.createResource(foafNS + user.getId());
-			Property nickProp = ResourceFactory.createProperty(foafNS, "nick");
+			Resource usedIdRes = ResourceFactory.createResource(FOAF_NS + user.getId());
+			Property nickProp = ResourceFactory.createProperty(FOAF_NS, "nick");
 			Literal nickLit = ResourceFactory.createTypedLiteral(user.getScreenName());
 			userModel.add(usedIdRes, nickProp, nickLit);
 
 			// STATUS COUNT
-			Property statusCountProp = ResourceFactory.createProperty(stNS, "statusCount");
+			Property statusCountProp = ResourceFactory.createProperty(SEMANTIC_TWEETER_NS, "statusCount");
 			Literal statusCountLit = ResourceFactory.createTypedLiteral(user.getStatusesCount());
 			userModel.add(usedIdRes, statusCountProp, statusCountLit);
 
 			// LOCATION
-			Property locationProp = ResourceFactory.createProperty(stNS, "location");
+			Property locationProp = ResourceFactory.createProperty(SEMANTIC_TWEETER_NS, "location");
 			Literal locationLit = ResourceFactory.createTypedLiteral(user.getLocation());
 			userModel.add(usedIdRes, locationProp, locationLit);
 
-			generalModel.add(userModel);
+			// add the latest tweets
+			ResponseList<Status> latestTweets = twitter.getUserTimeline(user.getId(), new Paging(1, 100));
+			Iterator<Status> statusIt = latestTweets.iterator();
+			while (statusIt.hasNext()) {
+				Status status = statusIt.next();
+				long statusId = status.getId();
+				Resource statusRes = ResourceFactory.createResource(SEMANTIC_TWEET_NS + statusId);
 
+				Property hasTweetProp = ResourceFactory.createProperty(SEMANTIC_TWEETER_NS, "hasTweet");
+				userModel.add(usedIdRes, hasTweetProp, statusRes);
+
+				Property isRetweetProp = ResourceFactory.createProperty(SEMANTIC_TWEET_NS, "isRetweet");
+				boolean isRetweet = status.isRetweet();
+				Literal isRetweetLit = ResourceFactory.createTypedLiteral(isRetweet);
+				userModel.add(statusRes, isRetweetProp, isRetweetLit);
+			}
+
+			generalModel.add(userModel);
 			File userFile = new File("userModels", user.getScreenName() + ".xml");
 			userModel.write(new FileOutputStream(userFile));
 
@@ -85,12 +108,14 @@ public class App {
 		}
 
 		StringBuilder queryStr = new StringBuilder();
-		queryStr.append("PREFIX foaf: <http://xmlns.com/foaf/0.1/Person#> ");
-		queryStr.append("PREFIX st: <http://twitter.com/semantic/Tweeter#> ");
+		queryStr.append("PREFIX foaf: <").append(FOAF_NS).append("> ");
+		queryStr.append("PREFIX stweeter: <").append(SEMANTIC_TWEETER_NS).append("> ");
+		queryStr.append("PREFIX stweet: <").append(SEMANTIC_TWEET_NS).append("> ");
 		queryStr.append("SELECT ?name ?count ");
 		queryStr.append("WHERE { ");
-		queryStr.append(" ?x foaf:nick ?name .");
-		queryStr.append(" ?x st:statusCount ?count");
+		queryStr.append(" ?x foaf:nick ?name . ");
+		queryStr.append(" ?x stweeter:statusCount ?count . ");
+		// queryStr.append(" ?x stweeter:hasTweet ?y . ");
 		queryStr.append("}");
 
 		QueryExecution query = QueryExecutionFactory.create(queryStr.toString(), generalModel);
